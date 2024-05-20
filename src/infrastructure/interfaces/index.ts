50a32b12d129cef0812c1a9c80695e96056ecf5c
eeburgosz <hdst.bur@gmail.com>
2024-05-20 18:39:46 -0300

    AuthStore - Zustand

--- src/infrastructure/interfaces/index.ts
+++ src/infrastructure/interfaces/index.ts
@@ -1,1 +1,2 @@
 export * from './auth.responses';
+export * from './auth.status';
---
+++ src/presentation/store/auth/useAuthStore.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+import { User } from '../../../domain/entities';
+import type { AuthStatus } from '../../../infrastructure/interfaces';
+import { authLogin } from '../../../actions/auth/auth';
+
+export interface AuthState {
+  status: AuthStatus;
+  token?: string;
+  user?: User;
+
+  login: (email: string, password: string) => Promise<boolean>;
+}
+
+export const useAuthStore = create<AuthState>()((set, get) => ({
+  status: 'checking',
+  token: undefined,
+  user: undefined,
+  login: async (email: string, password: string) => {
+    const resp = await authLogin(email, password);
+    if (!resp) {
+      set({ status: 'unauthenticated', token: undefined, user: undefined });
+      return false;
+    }
+    //Todo: Guardar el token en un storage persistente
+    set({ status: 'authenticated', token: resp.token, user: resp.user });
+    return true;
+  },
+}));
