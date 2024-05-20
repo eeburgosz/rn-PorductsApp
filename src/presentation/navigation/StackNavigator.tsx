d23ccad16a46036329c3ad9bd68c1cc202b62e49
eeburgosz <hdst.bur@gmail.com>
2024-05-20 16:29:03 -0300

    Diseño básico del login

--- src/presentation/navigation/StackNavigator.tsx
+++ src/presentation/navigation/StackNavigator.tsx
@@ -20,6 +20,7 @@ const Stack = createStackNavigator<RootStackParams>();
 export const StackNavigator = () => {
   return (
     <Stack.Navigator
+      initialRouteName="LoginScreen"
       screenOptions={{
         headerShown: false,
       }}>
--- src/presentation/screens/auth/LoginScreen.tsx
+++ src/presentation/screens/auth/LoginScreen.tsx
@@ -1,10 +1,65 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import {Button, Input, Layout, Text} from '@ui-kitten/components';
+import {useWindowDimensions} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export const LoginScreen = () => {
+  const {height} = useWindowDimensions();
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <Layout style={{flex: 1}}>
+      <ScrollView style={{marginHorizontal: 40}}>
+        <Layout style={{paddingTop: height * 0.35}}>
+          <Text category="h1">Ingresar</Text>
+          <Text category="p2">Por favor, ingrese para continuar</Text>
+        </Layout>
+
+        {/* Inputs */}
+        <Layout style={{marginTop: 20}}>
+          <Input
+            style={{marginBottom: 10}}
+            placeholder="Correo electrónico"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+
+          <Input
+            style={{marginBottom: 10}}
+            placeholder="Contraseña"
+            secureTextEntry
+            autoCapitalize="none"
+          />
+
+          {/* Space */}
+          <Layout style={{height: 20}} />
+
+          {/* Botón */}
+          <Layout>
+            <Button
+              onPress={() => {}}
+              // appearance="ghost"
+            >
+              Ingresar
+            </Button>
+
+            {/* Space */}
+            <Layout style={{height: 50}} />
+
+            {/* Información para crear cuenta */}
+            <Layout
+              style={{
+                alignItems: 'flex-end',
+                flexDirection: 'row',
+                justifyContent: 'center',
+              }}>
+              <Text>¿No tienes cuenta?</Text>
+              <Text status="primary" category="s1" onPress={() => {}}>
+                {' '}
+                Crea una
+              </Text>
+            </Layout>
+          </Layout>
+        </Layout>
+      </ScrollView>
+    </Layout>
   );
 };
