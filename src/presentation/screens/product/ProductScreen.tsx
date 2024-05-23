bd4300a2b64dfcc9753bef04ec01051f0ff33e90
eeburgosz <hdst.bur@gmail.com>
2024-05-23 18:35:27 -0300

    Diseño de la pantalla de producto

--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -1,10 +1,12 @@
-import { Text } from '@ui-kitten/components';
 import { MainLayout } from '../../layouts';
 import { useQuery } from '@tanstack/react-query';
 import { getProductById } from '../../../actions';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../../navigation/StackNavigator';
 import { useRef } from 'react';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
+import { Input, Layout } from '@ui-kitten/components';
+import { FadeInImage } from '../../components';
 
 interface Props extends StackScreenProps<RootStackParams, 'ProductScreen'> {}
 
@@ -27,7 +29,64 @@ export const ProductScreen = ({ route }: Props) => {
 
   return (
     <MainLayout title={product.title} subTitle={`Precio: ${product.price}`}>
-      <Text>Hola mundo</Text>
+      <ScrollView style={{ flex: 1 }}>
+        {/* Imágenes del producto */}
+        {/* Todo: Tener en consideración cuando no hay imágenes  */}
+        <Layout>
+          <FlatList
+            data={product.images}
+            keyExtractor={item => item}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <FadeInImage
+                uri={item}
+                style={{ width: 300, height: 300, marginHorizontal: 7 }}
+              />
+            )}
+          />
+        </Layout>
+        {/* Formulario */}
+        <Layout style={{ marginHorizontal: 10 }}>
+          <Input
+            label="Titulo"
+            value={product.title}
+            style={{ marginVertical: 5 }}
+          />
+          <Input
+            label="Slug"
+            value={product.slug}
+            style={{ marginVertical: 5 }}
+          />
+          <Input
+            label="Descripción"
+            value={product.description}
+            multiline
+            numberOfLines={5}
+            style={{ marginVertical: 5 }}
+          />
+        </Layout>
+        <Layout
+          style={{
+            marginVertical: 5,
+            marginHorizontal: 15,
+            flexDirection: 'row',
+            gap: 10,
+          }}>
+          <Input
+            label="Precio"
+            value={product.price.toString()}
+            style={{ flex: 1 }}
+          />
+          <Input
+            label="Inventario"
+            value={product.stock.toString()}
+            style={{ flex: 1 }}
+          />
+        </Layout>
+
+        <Layout style={{ height: 200 }} />
+      </ScrollView>
     </MainLayout>
   );
 };
