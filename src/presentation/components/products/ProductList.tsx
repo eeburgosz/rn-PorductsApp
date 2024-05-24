4b8ccdd3f0588d87f4aa534180fd3101d11e4eba
eeburgosz <hdst.bur@gmail.com>
2024-05-24 17:19:48 -0300

    Actualizar caché - PullToRefresh

--- src/presentation/components/products/ProductList.tsx
+++ src/presentation/components/products/ProductList.tsx
@@ -1,8 +1,9 @@
-import { Layout, List, Text } from '@ui-kitten/components';
+import { useState } from 'react';
+import { Layout, List } from '@ui-kitten/components';
 import { Product } from '../../../domain/entities/product.entity';
 import { ProductCard } from './ProductCard';
-import { useState } from 'react';
 import { RefreshControl } from 'react-native-gesture-handler';
+import { useQueryClient } from '@tanstack/react-query';
 interface Props {
   products: Product[];
   fetchNextPage: () => void;
@@ -11,12 +12,15 @@ interface Props {
 }
 
 export const ProductList = ({ products, fetchNextPage }: Props) => {
+  const queryClient = useQueryClient();
+
   const [isRefreshing, setIsRefreshing] = useState(false);
 
   const onPullToRefresh = async () => {
     setIsRefreshing(true);
+    queryClient.invalidateQueries({ queryKey: ['products', 'infinite'] });
     //Sleep 2 seconds
-    await new Promise(resolve => setTimeout(resolve, 1500));
+    await new Promise(resolve => setTimeout(resolve, 200));
     setIsRefreshing(false);
   };
 
