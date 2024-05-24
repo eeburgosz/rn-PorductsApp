4ce83130fff7bc6e040f2adb0f83d65088b39000
eeburgosz <hdst.bur@gmail.com>
2024-05-24 17:32:47 -0300

    Componente FAB

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,4 +1,5 @@
 export * from './ui/MyIcon';
 export * from './ui/FullScreenLoader';
 export * from './ui/FadeInImage';
+export * from './ui/FAB';
 export * from './products/ProductList';
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,13 @@
 import { getProductsByPage } from '../../../actions';
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { MainLayout } from '../../layouts';
-import { FullScreenLoader, ProductList } from '../../components';
+import { FAB, FullScreenLoader, ProductList } from '../../components';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { RootStackParams } from '../../navigation/StackNavigator';
 
 export const HomeScreen = () => {
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
   // const { isLoading, data: products = [] } = useQuery({
   //   queryKey: ['products', 'infinite'],
   //   staleTime: 1000 * 60 * 60,
@@ -23,15 +27,26 @@ export const HomeScreen = () => {
   //! data regresa un arreglo de arreglos porque trae la información por páginas. Toda la información es un arreglo general y cada sub arreglo es de productos por página. [  [p1,p2,p3],[p4,p5,p6],[p7,p8,p9]  ]
 
   return (
-    <MainLayout title="TesloShop Products" subTitle="Aplicación administrativa">
-      {isLoading ? (
-        <FullScreenLoader />
-      ) : (
-        <ProductList
-          fetchNextPage={fetchNextPage}
-          products={data?.pages.flat() ?? []}
-        />
-      )}
-    </MainLayout>
+    <>
+      <MainLayout
+        title="TesloShop Products"
+        subTitle="Aplicación administrativa">
+        {isLoading ? (
+          <FullScreenLoader />
+        ) : (
+          <ProductList
+            fetchNextPage={fetchNextPage}
+            products={data?.pages.flat() ?? []}
+          />
+        )}
+      </MainLayout>
+      <FAB
+        style={{ position: 'absolute', bottom: 20, right: 20 }}
+        onPress={() =>
+          navigation.navigate('ProductScreen', { productId: 'new' })
+        }
+        iconName="plus-outline"
+      />
+    </>
   );
 };
